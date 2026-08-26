4eaae045ccaf5829f3ffef780bf252be4a162415
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-08-26 12:50:18 +0100

    Home Page ListView

--- feed/models.py
+++ feed/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
-    
+    date = models.DateField(auto_now=True)
     
     def __str__(self):
         return self.text[0:100]
