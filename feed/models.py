04d8963ab64b6da55f1d7088216b9bf131ddf885
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-08-27 15:11:51 +0100

    Tailwind styled posts with a error0.2

--- feed/models.py
+++ feed/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
-    date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.text[0:100]
