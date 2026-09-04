5f0e671babafaff056bb2440d6898fd628bc3771
Aliga Darin Ubaoma <daroaliga@gmail.com>
2026-09-04 04:59:39 +0100

    Ajaxing new post

--- feed/frontend/js/main.js
+++ feed/frontend/js/main.js
@@ -29,5 +29,46 @@ $.ajaxSetup({
 });
 $(document).on("click", ".js-toggle-modal", function(e){
     e.preventDefault()
-    $(".js-model").toggleClass("hidden")
-});
+    $(".js-modal").toggleClass("hidden")
+})
+.on("click",".js-submit",function(e){
+    e.preventDefault()
+    const text = $(".js-post-text").val().trim()
+
+    if(!text.length){
+        return false
+    }
+    $(".js-modal").addClass("hidden")
+    $(".js-post-text").val('')
+
+    .on("click", ".js-submit", function(e) {
+    e.preventDefault()
+    const text = $(".js-post-text").val().trim()
+    const $btn = $(this)
+
+    if(!text.length) {
+        return false
+    }
+
+    $btn.prop("disabled", true).text("Posting!")
+    $.ajax({
+        type: 'POST',
+        url: $(".js-post-text").data("post-url"),
+        data: {
+            text: text
+        },
+        success: (dataHtml) => {
+            $(".js-modal").addClass("hidden");
+            $("#posts-container").prepend(dataHtml);
+            $btn.prop("disabled", false).text("New Post");
+            $(".js-post-text").val('')
+        },
+        error: (error) => {
+            console.warn(error)
+            $btn.prop("disabled", false).text("Error");
+        }
+    });
+})
+
+
+})
